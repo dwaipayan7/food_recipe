0bb3194950a9aaa99b04a33eff9dbbe1bf24ffd3
Dwaipayan Biswas <biswasdwai007@gmail.com>
2023-07-30 10:14:20 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height ,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff213A50),
+                  Color(0xff071938)
+                ]
+              )
+            ),
+          ),
+          SafeArea(
+            child: Text("Hello I'm Home Page",
+            style: TextStyle(color: Colors.white),),
+          )
+        ],
+      ),
+
+    );
+  }
+}
